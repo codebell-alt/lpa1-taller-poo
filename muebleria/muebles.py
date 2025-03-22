6e06699a5a39c6ce7fef00549413f69ef2c24439
codebell <isabella315784@gmail.com>
2025-03-22 10:35:31 -0500

    Se agrega la clase abstracta Mueble

--- muebleria/muebles.py
+++ muebleria/muebles.py
@@ -1,1 +1,11 @@
 # clases relacionadas con los muebles
+from abc import ABC, abstractmethod
+
+class Mueble(ABC):
+    def __init__(self, material, precio):
+        self.material = material
+        self.precio = precio
+
+    @abstractmethod
+    def calcular_precio_final(self):
+        pass
