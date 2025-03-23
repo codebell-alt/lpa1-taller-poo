945a96846ef621b0457106257912fd68059d2ed8
codebell <isabella315784@gmail.com>
2025-03-23 11:56:27 -0500

    Se añade pruebas unitarias con pytest

--- tests/test_muebles.py
+++ tests/test_muebles.py
@@ -1,2 +1,15 @@
 # pruebas unitarias utilizando pytest
+import pytest
+from muebleria.muebles import Silla, Mesa, Armario
 
+def test_silla():
+    silla = Silla("Madera", 50, True)
+    assert silla.calcular_precio_final() == pytest.approx(55)  
+
+def test_mesa():
+    mesa = Mesa("Metal", 100, "Grande")
+    assert mesa.calcular_precio_final() == pytest.approx(120)
+
+def test_armario():
+    armario = Armario("Plástico", 200, 2)
+    assert armario.calcular_precio_final() == pytest.approx(230)
